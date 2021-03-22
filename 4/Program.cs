081d301294e9be1d52035e385676acbdcb56710c
stas <stas-723@ya.ru>
2021-03-23 02:42:08 +0300

    дз

---
+++ 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число А: ");
+            double A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число В: ");
+            double B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число C: ");
+            double C = Convert.ToInt32(Console.ReadLine());
+
+            double x;
+
+            {
+
+
+
+                double X = (C - B) / A;
+
+
+                Console.WriteLine($"A * X + B = C  X = {X}");
+
+            }
+
+        }
+    }
+}
