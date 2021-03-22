26195b9eb16b8208e5e25d1a09c1568d6197a89c
stas <stas-723@ya.ru>
2021-03-23 02:45:19 +0300

    дз 5

---
+++ 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты X1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты Y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты X2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты Y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            int a = (y1 - y2) / (x1 - x2);
+            int b = y2 - a * x2;
+
+            Console.WriteLine($"y = {a}x + {b}");
+        }
+    }
+}
