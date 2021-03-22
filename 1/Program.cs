0dbd3318a7b2521abbb01db174bd46e587d03121
stas <stas-723@ya.ru>
2021-03-23 02:01:27 +0300

    дз

---
+++ 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            {
+                Console.WriteLine("Введите число A");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите число B");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+
+
+                Console.WriteLine(5 * a + (b * b) / (b - a));
+            }
+        }
+    }
+}
