7118e08cccdf75ac6ae37d3f70e59fea512e2f7f
stas <stas-723@ya.ru>
2021-03-23 02:40:36 +0300

    дз

---
+++ 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _3
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число А: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число В: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            int x; int x1;
+
+            {
+
+
+                x = A / B;
+                x1 = A % B;
+
+
+
+                Console.WriteLine($"результат деления:{x}");
+                Console.WriteLine($"остаток:{x1}");
+            }
+
+        }
+    }
+}
